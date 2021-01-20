236e2951b34e2f886c48f06db9583086aefe0e10
MakenzieHale <makenzielhale@gmail.com>
2021-01-19 21:54:59 -0500

    ..

--- quizex.js
+++ quizex.js
@@ -16,27 +16,39 @@ for(const [question,answer] of quiz){
 
 //Chapter 4 example
 
-const quiz = [["What is Superman's real name?", "Clark Kent"],
+/*const quiz = [["What is Superman's real name?", "Clark Kent"],
     ["What is Wonder Woman's real name?", "Diana Prince"],
     ["What is Batman's real name?","Bruce Wayne"]
+];*/
+
+//Chapter 5 examples
+
+const quiz = [ 
+    {name: "Superman", realName: "Clark Kent"},
+    {name: "Wonder Woman", realName: "Diana Prince"},
+    {name: "Batman", realName: "Bruce Wayne"},
 ];
-function start(quiz){ // main game function, contains all of the steps of the game.
-    let score = 0;
+const game = {
+ start(quiz){ // main game function, contains all of the steps of the game.
+     this.questions = [...quiz];
+      this.score = 0;
 
     //main game loop
-    for(const [question,answer] of quiz){
-        const response = ask(question);
-        check(response,answer);
+    for(const question of this.questions){
+      this.question = question;
+      this.ask();
     }
     //end main loop
-    gameOver();
+    this.gameOver();
+},
+    ask(){
+        const question = `What is ${this.question.name}'s real name?`;
+        const response = prompt(question);
+        this.check(response);
+    },
 
-    //function declarations
-    function ask(question){
-        return prompt(question)
-    }
-
-    function check(response,answer){
+     check(response){
+         const answer = this.question.realName;
         if(response == answer){
             alert('Correct');
             score++
@@ -44,10 +56,12 @@ function start(quiz){ // main game function, contains all of the steps of the ga
             alert(`Wrong! The correct answer was ${answer}`);
 
         }
+    },
         
-        function gameOver(){
+         gameOver(){
             alert(`Game Over, you scored ${score} point${score !== 1 ? 's' : ' '}`);
         }
     }
-    start(quiz);
-}
+    game.start(quiz);
+
+
