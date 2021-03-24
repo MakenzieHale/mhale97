d72d2d4eb7d79d3766c1f6143b5203bffe50b81c
MakenzieHale <makenzielhale@gmail.com>
2021-03-23 21:38:40 -0400

    ron-swanson-app

--- final-project/ronSwanson.js
+++ final-project/ronSwanson.js
@@ -17,14 +17,15 @@ function getJSON (response) {
 function fetchQuote (){
 fetch(endpoint)
 .then(getJSON)
+.then(checkQuote)
 .then(showQuote)
 .catch(showError);
 }
 button.addEventListener("click", fetchQuote);
 
 //display the quote
-function showQuote (quotes){
-    quote.textContent = quotes[0];
+function showQuote (quoteString){
+    quote.textContent = quoteString;
 }
 
 //if error
@@ -35,4 +36,22 @@ function showError(){
         "<strong>" + "sorry, there was a problem getting your Ron Swanson quote, please try again later "
         + "</strong" + "</p>"
     );
+}
+
+//avoid duplicate quotes
+
+var pastQuotes = [];
+//check if current quote is valid. If quote was among the last 50, function will recursively call itself until a new quote is found.
+
+function checkQuote (data){
+    if (pastQuotes.includes(data[0])){
+        return fetch(endpoint).then(getJSON).then(checkQuote);
+    }
+
+    if(pastQuotes.length === 50) {
+        pastQuotes = [];
+    }
+    pastQuotes.push(data[0]);
+
+    return data[0];
 }
